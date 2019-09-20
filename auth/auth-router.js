ba9f722881e3ba62f2a8df7086b0678a91106cc1
VulpineReynard <milare8@gmail.com>
2019-09-20 12:49:50 -0400

    register/login/jokes endpoint fully functional, will add extra middleware & functionality with extra time

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Users = require('../users/users-model.js');
+const secrets = require('../config/secrets.js');
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
   let user = req.body;
@@ -22,15 +24,19 @@ router.post('/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
+      
       // check password is correct
-      if (user && bcrypt.compareSync(password, user.password)) {
+      if (!user || !bcrypt.compareSync(password, user.password)) {
+        res.status(401).json({ message: 'Invalid Credentials' });
+        
+      } else {
+        console.log(user)
         const token = generateToken(user)
+        
         res.status(200).json({
           message: `Welcome ${user.username}!, have a token...`,
           token, // attach the token as part of the response
         });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
       }
     })
     .catch(error => {
