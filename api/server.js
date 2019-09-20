f61ad32a2d4bc68c85169622b66b8086c484a844
VulpineReynard <milare8@gmail.com>
2019-09-20 12:10:09 -0400

    server is up and sanity is good

--- api/server.js
+++ api/server.js
@@ -15,4 +15,8 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
+server.get('/', (req, res) => {
+  res.send({ message: "Good to go." })
+})
+
 module.exports = server;
